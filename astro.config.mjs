d3f0ae275d5b2e110f1337fc423d5a640e0f228b
Bogdan Jovanovic <bogdanjovanovic@cloud.upwork.com>
2022-10-23 00:02:32 +0200

    Remove base config parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://bogyj.github.io',
+    site: 'https://bogyj.github.io'
 
     // Don’t set a base parameter if:
     // Your repository is named <YOUR_USERNAME>.github.io.
     // You’re using a custom domain.
-    base: '/resume-website'
+    // base: '/resume-website'
 });
